7d86548dec5114f43381f3dbf51ac4ec4852d7bf
manav-kasare <kmanav0705@gmail.com>
2021-10-13 19:46:24 +0530

    FeedPost sorting

--- pages/feed/feedpost.tsx
+++ pages/feed/feedpost.tsx
@@ -2,9 +2,37 @@ import React, { useEffect, useState, useCallback } from 'react';
 import { View, FlatList, RefreshControl } from 'react-native';
 import Post from '../../components/feed/post';
 import { firestore } from '../../firebase';
+
 const FeedPost = ({ category }) => {
-  const [posts, setposts] = useState([]);
-  const [refresh, setrefresh] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
+  const [refresh, setrefresh] = useState(false);
+  const [firstRender, setFirstRender] = useState(true);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    servercall();
+  }, []);
+
+  useEffect(() => {
+    if (!firstRender && category) handleFilterData();
+    if (!category) handleUnfilterData();
+  }, [category]);
+
+  useEffect(() => {
+    setrefresh(false);
+  }, [filteredPosts]);
+
+  const handleFilterData = () => {
+    const filtered = posts.filter(
+      ({ type }) => type === category.toLowerCase()
+    );
+    setFilteredPosts(filtered);
+    setLoading(false);
+  };
+
+  const handleUnfilterData = () => setFilteredPosts(posts);
+
   const servercall = useCallback(async () => {
     let data = [];
     await firestore
@@ -17,29 +45,34 @@ const FeedPost = ({ category }) => {
           y.id = x.id;
           data.push(y);
         });
-        setposts(data);
+        setFirstRender(false);
+        setPosts(data);
+        setFilteredPosts(data);
+        console.log('posts', data);
         setrefresh(false);
       });
   }, []);
-  useEffect(() => {
-    servercall();
-  }, []);
+
+  const refreshControl = (
+    <RefreshControl
+      refreshing={refresh}
+      onRefresh={async () => {
+        servercall();
+      }}
+    />
+  );
+
+  const renderItem = ({ item }) => {
+    return <Post name={item.name} data={item.data} type={item.type} />;
+  };
+
   return (
     <View style={{ backgroundColor: '#1f2232', flex: 1 }}>
       <FlatList
-        data={posts}
-        refreshControl={
-          <RefreshControl
-            refreshing={refresh}
-            onRefresh={async () => {
-              servercall();
-            }}
-          />
-        }
+        data={filteredPosts}
+        refreshControl={refreshControl}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => {
-          return <Post name={item.name} data={item.data} type={item.type} />;
-        }}
+        renderItem={renderItem}
       />
     </View>
   );
