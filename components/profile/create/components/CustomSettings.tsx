55110f4f2f2ed713c09cded639041933542fc83d
manav-kasare <kmanav0705@gmail.com>
2021-10-20 20:49:24 +0530

    custom settings basic UI

--- components/profile/create/components/CustomSettings.tsx
+++ components/profile/create/components/CustomSettings.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import theme from '../../../../theme';
+import ChevronDown from '../../../icons/ChevronDown';
 import Info from '../../../icons/Info';
 
 export default function CustomSettings({ navigation, route: { params } }) {
@@ -20,7 +21,38 @@ export default function CustomSettings({ navigation, route: { params } }) {
         </Text>
       </Text>
 
-      <View style={styles.container}></View>
+      <View style={styles.container}>
+        <View style={styles.tile}>
+          <Text style={[styles.label, styles.active]}>Country</Text>
+          <ChevronDown color="#fff" />
+        </View>
+
+        <View style={styles.tile}>
+          <Text style={[styles.label, styles.inactive]}>State</Text>
+          <ChevronDown color="#363C5A" />
+        </View>
+
+        <View style={styles.tile}>
+          <Text style={[styles.label, styles.inactive]}>Pincode(s)</Text>
+          <ChevronDown color="#363C5A" />
+        </View>
+
+        <View style={styles.tile}>
+          <Text style={[styles.label, styles.inactive]}>Age</Text>
+          <ChevronDown color="#363C5A" />
+        </View>
+
+        <View style={styles.tile}>
+          <Text style={[styles.label, styles.inactive]}>Gender</Text>
+          <ChevronDown color="#363C5A" />
+        </View>
+      </View>
+
+      <View style={styles.bottom}>
+        <TouchableOpacity style={styles.bottomButton}>
+          <Text style={styles.buttonLabel}>Submit</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -41,5 +73,41 @@ const styles = StyleSheet.create({
   },
   container: {
     marginTop: theme.height * 0.1,
+    flex: 1,
+    alignItems: 'center',
+  },
+  tile: {
+    width: theme.width * 0.8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 20,
+  },
+  label: {
+    fontSize: 18,
+  },
+  active: {
+    color: '#fff',
+  },
+  inactive: {
+    color: '#363C5A',
+  },
+  bottom: {
+    height: theme.height * 0.05,
+    alignItems: 'flex-end',
+  },
+  bottomButton: {
+    backgroundColor: '#FFB30F',
+    width: theme.width * 0.3,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginTop: 30,
+  },
+  buttonLabel: {
+    fontSize: 14,
+    fontFamily: 'Poppins-Regular',
+    color: theme.background.primary100,
   },
 });
